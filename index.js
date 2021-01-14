8d44ddd9b968136e1436a1f2f19c427a82577bb4
calavell <calavell@tcd.ie>
2021-01-14 18:02:14 +0000

    attempting to integrate weather api

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ $(document).ready(function() {
       updatePowerSavingMode();
     })
 
+    // let getReturnValue = $.get("https://api.openweathermap.org/data/2.5/weather?q=london&appid=9ec9591a31e3be7446a43513c920d793", (weather) => {
+    //   weather.forEach((item) => console.log(item));
+    // })
+    // console.log(getReturnValue.weather);
+    //
+    // const apiRes = await fetch( `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=9ec9591a31e3be7446a43513c920d793` ); 
+
 
 
 
